a64b107f8da4c42c29cc3395f9ee2bc5f394f70f
mumugisha <prudent.nkubito@gmail.com>
2024-10-26 23:03:02 +0300

    Pagination

--- 0-simple_helper_function.py
+++ 0-simple_helper_function.py
@@ -6,11 +6,16 @@ and end indexes for pagination given a page and page_size.
 
 from typing import Tuple
 
-
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calculates the start and end indexes for pagination.
 
+    Parameters:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end indexes for pagination.
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
