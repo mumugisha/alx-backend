531dd0b2180682d27fb46cf32a10626c014f30a9
mumugisha <prudent.nkubito@gmail.com>
2024-10-26 23:00:32 +0300

    Pagination

--- 0-simple_helper_function.py
+++ 0-simple_helper_function.py
@@ -12,6 +12,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Calculates the start and end indexes for pagination.
 
     """
-    start_index = (page - 1) + page_size
-    end_index = page + page_size
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
     return (start_index, end_index)
