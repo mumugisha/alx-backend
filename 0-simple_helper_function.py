5c414b4feb23452f200818ebd7dc3cc750ce2f56
mumugisha <prudent.nkubito@gmail.com>
2024-10-26 22:12:34 +0300

    Pagination

--- 0-simple_helper_function.py
+++ 0-simple_helper_function.py
@@ -13,5 +13,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
     """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = page + page_size
     return (start_index, end_index)
