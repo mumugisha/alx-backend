d2e137529e3e375889b81996e6aa5c42780236f6
mumugisha <prudent.nkubito@gmail.com>
2024-10-28 23:00:59 +0200

    Pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
-Deletion-resilient hypermedia pagination
+The goal here is that if between two queries, certain
+rows are removed from the dataset,
+the user does not miss items from dataset when changing page
 """
-
 import csv
 from typing import List, Dict, Any
 
