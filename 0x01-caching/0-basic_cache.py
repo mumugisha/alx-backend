eccbb10d19cfbeadab9dca2156d7382c13231e42
mumugisha <prudent.nkubito@gmail.com>
2024-10-30 00:01:09 +0200

    Caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" BasicCache module """
+
+from Base_Caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class that inherits from BaseCaching and provides a simple caching system.
+    This caching system has no limit on the number of items it can store.
+    """
+
+    def put(self, key, item):
+        """
+        Assigns the item value to the key in the cache_data dictionary.
+        If key or item is None, it does nothing.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Returns the value linked to key in cache_data.
+        If key is None or does not exist, returns None.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
