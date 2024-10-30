a8bb73ac2dda8af1efef33f6a83258d197daf3e2
mumugisha <prudent.nkubito@gmail.com>
2024-10-30 13:44:47 +0200

    Pagination

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
-""" BaseCaching module
-"""
+""" BaseCaching module """
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ LRU Cache defines the caching system
-    """
+    """ LRU Cache defines the caching system """
 
     def __init__(self):
-        """ Initialize
-        """
+        """ Initialize """
         super().__init__()
         self.usage = []
 
     def put(self, key, item):
-        """ Add an item to the cache
-        """
+        """ Add an item to the cache """
         if key is None or item is None:
             pass
         else:
@@ -31,8 +27,7 @@ class LRUCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
-        """
+        """ Get an item by key """
         if key is None or key in self.cache_data.keys():
             del self.usage[self.usage.index(key)]
             self.usage.append(key)
