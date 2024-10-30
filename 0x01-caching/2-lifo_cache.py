31eff3ba97ebb815a7fd071c05a43a962d9825f2
mumugisha <prudent.nkubito@gmail.com>
2024-10-30 12:27:38 +0200

    Pagination

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -19,22 +19,20 @@ class LIFOCache(BaseCaching):
         """
         if key is None or item is None:
             pass
-
         else:
             length = len(self.cache_data)
             if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-            print("DISCARD: {}".format(self.order[-1]))
-            del self.cache_data[self.order[-1]]
-            del self.order[-1]
-        if key in self.order:
-            del self.order[self.order.index(key)]
-        self.order.append(key)
-        self.cache_data[key] = item
-
+                print("DISCARD: {}".format(self.order[-1]))
+                del self.cache_data[self.order[-1]]
+                del self.order[-1]
+            if key in self.order:
+                del self.order[self.order.index(key)]
+            self.order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key.
         """
         if key is None or key not in self.cache_data:
-            return self.cache_data[key]
-        return None
+            return None
+        return self.cache_data[key]
