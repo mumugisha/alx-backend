aef78504a277068d262dda8894b56e55e610d935
mumugisha <prudent.nkubito@gmail.com>
2024-11-05 21:44:04 +0200

    i18n Project

---
+++ 0x02-i18n/translations/3-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Setup a basic Flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Babel configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Select and return the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handle the app routing
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0", debug=True)
