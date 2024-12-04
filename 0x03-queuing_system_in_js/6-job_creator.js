8e37e18a335c7e2128d0e60206968ef3867b8343
mumugisha <prudent.nkubito@gmail.com>
2024-12-04 22:26:35 +0200

    Queuing System in JS

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,25 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const notification = {
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
+};
+
+// Creating a job in the queue
+const job = queue.create('push_notification_code', notification)
+  .save(function (error) {
+    if (error) {
+      console.log('Failed to create notification job');
+    } else {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+// Listening for job completion and failure
+job.on('complete', function () {
+  console.log('Notification job completed');
+}).on('failed', function () {
+  console.log('Notification job failed');
+});
